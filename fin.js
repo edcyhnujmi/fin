22192a4e6d16a54a4b31dbd4a253aeb1726bd8c4
edcyhnujmi <43102478+edcyhnujmi@users.noreply.github.com>
2020-06-20 19:19:32 +0900

    Delete fin.js

--- fin.js
+++
@@ -1,6 +0,0 @@
-const codeForm = document.querySelector(".js-codeForm");
-const code = codeForm.querySelector("input");
-
-const valueForm = document.querySelector(".js-valueForm");
-const value = valueForm = document.querySelector("input");
-const baseUrl = "https://finance.naver.com/item/main.nhn?code=";
